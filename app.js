c74c1f03bc62869862c80d7f44cb4b9333148622
Pedro Carvalho <eng.pedro.amb@gmail.com>
2023-06-06 15:29:52 +0200

    latest changes

--- app.js
+++ app.js
@@ -3,16 +3,7 @@ import retweet from "./index.js";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send(
-    "<div style='color:blue;text-align:center;height:5000px;'> \
-  <span> \
-  <p> \
-  Javascript \
-  </p> \
-  </div>"
-  );
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
